c32afa41500ea834913293d567c451d9e3537202
Denmas12 <m.jawahir48@gmail.com>
2023-10-12 08:36:35 +0700

    First

---
+++ app/middleware/index.js
@@ -0,0 +1,11 @@
+const authJwt = require("./authJwt");
+const verifySignUp = require("./verifySignUp");
+const uploadIzin = require("./uploadIzin");
+const uploadAbsen = require("./uploadAbsen");
+
+module.exports = {
+  authJwt,
+  verifySignUp,
+  uploadIzin,
+  uploadAbsen
+};
